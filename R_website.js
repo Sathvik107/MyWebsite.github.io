2eb5dcd0df843d16268fae6f3541ef18909bb211
Sathvik107 <103804079+Sathvik107@users.noreply.github.com>
2022-06-08 19:40:32 +0530

    Update R_website.js

--- R_website.js
+++ R_website.js
@@ -50,7 +50,7 @@ function create_account(){
     var p=document.getElementById("p1").value;  
     var cp=document.getElementById("p2").value;  
             var letters = /^[A-Za-z]+$/;  
-            var email_val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z]\.{2,4})+$/;   
+            var email_val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z]{2,4})+$/;   
     if(n==''||e==''||p==''||cp==''){  
     alert("Enter your complete details carefully");  
     }  
